a49256f12fcabe18742d13cf9557d14e4bb14ffa
Yannis Angelis <ioannis.angelis@cern.ch>
2025-05-16 14:22:19 +0200

    Update build example

--- examples/build_hl/build_hl.py
+++ examples/build_hl/build_hl.py
@@ -3,18 +3,27 @@ import xtrack as xt
 from cpymad.madx import Madx
 
 # %%
-# optics_name = "../../../acc-models-lhc/strengths/round/opt_round_150_1000.madx"
-# optics_name = "../../../acc-models-lhc_orig/strengths/round/start_collapse/opt_collapse_1320_1500.madx"
-optics_name = "../../../acc-models-lhc_orig/strengths/round/start_collapse/opt_collapse_1000_1500.madx"
+# opt_path = "../../../hl16"
+opt_path = "../../../hl19"
+# optics_name = f"{opt_path}/strengths/ramp/opt_ramp_6000.madx"
 
+optics_name = f"{opt_path}/strengths/round/opt_round_150_1500.madx"
+
+# optics_name = f"{opt_path}/strengths/round/opt_round_150_1000.madx"
+# optics_name = "../../../acc-models-lhc_orig/strengths/round/start_collapse/opt_collapse_1320_1500.madx"
+# optics_name = f"{opt_path}/strengths/round/start_collapse/opt_collapse_1000_1500.madx"
+
+# optics_name = "../../../summer_optics/collapse/opt_collapse_1000_1500.madx"
+
+nrj = 6800.0
 # %%
 mad = Madx(stdout=False)
-mad.call("../../../acc-models-lhc/lhc.seq")
-mad.call("../../../acc-models-lhc/hllhc_sequence.madx")
-mad.call("../../../acc-models-lhc/hllhc_sequence.madx")
+mad.call(f"{opt_path}/lhc.seq")
+mad.call(f"{opt_path}/lhc_hl19.seq")
+mad.call(f"{opt_path}/hllhc_sequence.madx")
 mad.call(optics_name)
-mad.input("beam, sequence=lhcb1, particle=proton, energy=7000;")
-mad.input("beam, sequence=lhcb2, particle=proton, energy=7000,bv=-1;")
+mad.input(f"beam, sequence=lhcb1, particle=proton, energy={nrj};")
+mad.input(f"beam, sequence=lhcb2, particle=proton, energy={nrj},bv=-1;")
 mad.use("lhcb1")
 mad.use("lhcb2")
 
@@ -23,16 +32,17 @@ lhcb1_ref = xt.Line.from_madx_sequence(mad.sequence.lhcb1, deferred_expressions=
 lhcb2_ref = xt.Line.from_madx_sequence(mad.sequence.lhcb2, deferred_expressions=True)
 
 # %%
-particle_ref = xt.Particles(mass0=xt.PROTON_MASS_EV, p0c=7000e9)
+particle_ref = xt.Particles(mass0=xt.PROTON_MASS_EV, p0c=nrj * 1e9)
 lhcb1_ref.particle_ref = particle_ref
 lhcb2_ref.particle_ref = particle_ref
 # %%
 tw_refb1 = lhcb1_ref.twiss4d()
 tw_refb2 = lhcb2_ref.twiss4d()
 
-print("line beam1 qx", tw_refb1.qx)
-print("line beam2 qx", tw_refb2.qx)
-
+print("beam1 qx", tw_refb1.qx, "beam1 qy", tw_refb1.qy)
+print("beam2 qx", tw_refb2.qx, "beam2 qy", tw_refb2.qy)
+# %%
+tw_refb1.rows["ip.*"].cols["betx bety"]
 # %%
 env = xt.Environment(lines={"lhcb1": lhcb1_ref, "lhcb2": lhcb2_ref})
 env.lhcb1.twiss_default["method"] = "4d"
@@ -40,6 +50,8 @@ env.lhcb2.twiss_default["method"] = "4d"
 env.lhcb2.twiss_default["reverse"] = True
 
 # %%
+# env.to_json("hl19_inj.json")
+env.to_json("hl19_round_150_1500.json")
 
-env.to_json("hl_coll.json")
+# env.to_json("hl_round_150.json")
 # %%
